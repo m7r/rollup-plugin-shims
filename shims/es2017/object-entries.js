f8660acf055be835f85a421cf186f1602da1c2d9
Martin R. Hufsky <martin@malamaglo.de>
2019-08-15 08:01:42 +0200

    fix: Object.{values, entries}
    Support objects without prototype

--- shims/es2017/object-entries.js
+++ shims/es2017/object-entries.js
@@ -1,9 +1,12 @@
 if (!Object.entries) {
   Object.entries = function entries (O) {
+    if (O == null) throw new TypeError('Cannot convert undefined or null to object')
     var obj = Object(O)
+    var has = Object.prototype.hasOwnProperty
+    var enumerable = Object.prototype.propertyIsEnumerable
     var entrys = []
     for (var key in obj) {
-      if (obj.hasOwnProperty(key) && obj.propertyIsEnumerable(key)) {
+      if (has.call(obj, key) && enumerable.call(obj, key)) {
         entrys.push([key, obj[key]])
       }
     }
--- shims/es2017/object-values.js
+++ shims/es2017/object-values.js
@@ -1,9 +1,12 @@
 if (!Object.values) {
   Object.values = function values (O) {
+    if (O == null) throw new TypeError('Cannot convert undefined or null to object')
     var obj = Object(O)
+    var has = Object.prototype.hasOwnProperty
+    var enumerable = Object.prototype.propertyIsEnumerable
     var vals = []
     for (var key in obj) {
-      if (obj.hasOwnProperty(key) && obj.propertyIsEnumerable(key)) {
+      if (has.call(obj, key) && enumerable.call(obj, key)) {
         vals.push(obj[key])
       }
     }
