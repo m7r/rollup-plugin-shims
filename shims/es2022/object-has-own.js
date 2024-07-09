6ebb900b26c862692f1012b6261d588772a11bfe
Martin R. Hufsky <martin@malamaglo.de>
2024-07-09 14:37:18 +0200

    fix(shim): Object.hasOwn()

--- shims/es2022/object-has-own.js
+++ shims/es2022/object-has-own.js
@@ -1,7 +1,7 @@
 if (!Object.hasOwn) {
   Object.hasOwn = function hasOwn(obj, prop) {
     var O = coerceObject(obj);
-    var P = toStr(prop);
+    var P = String(prop);
     return has.call(O, P);
   };
 }
